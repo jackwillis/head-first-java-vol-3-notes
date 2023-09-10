04d2d91cd2b157654971d6b94e74cfa1be81fe39
Jack Willis <jack@attac.us>
2023-09-10 13:27:37 -0500

    Add rest of outline for QuizCardBuilder

--- src/quizcardbuilder/QuizCardBuilder.java
+++ src/quizcardbuilder/QuizCardBuilder.java
@@ -8,6 +8,22 @@ public class QuizCardBuilder {
 
     public void go() {
         System.out.println("QuizCardBuilder");
-        // build gui
+        // build and display gui
+    }
+
+    private void nextCard() {
+        // add the current card to the list
+        // and clear the text areas
+    }
+
+    private void saveCard() {
+        // bring up a file dialog box
+        // let the user name and save the set
+    }
+
+    private void saveFile(File file) {
+        // iterate through the list of cards and write
+        // each one out to a text file in a parseable way
+        // (in other words, with clear separations between parts)
     }
 }
